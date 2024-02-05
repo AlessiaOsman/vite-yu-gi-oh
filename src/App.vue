db691557915f40e3e6e22c5afb86b9b2876aa7eb
AlessiaOsman <alessia.osman@gmail.com>
2024-02-05 16:14:51 +0100

    installed bootstrap and Axios and created AppMain Component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script>
-
+  import AppMain from './components/AppMain.vue'
+  export default {
+    name: 'App',
+    components: {AppMain}
+  }
 </script>
 
 <template>
-  
+  <AppMain/>
 </template>
 
 <style lang="scss">
